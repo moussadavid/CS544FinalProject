c4303bdd43f6cbe9ea276b202948d581f56c9758
bernietes <bernietes0@gmail.com>
2022-09-22 20:03:44 -0600

    adds feign client

---
+++ Posts/src/main/java/edu/mum/cs544/integration/CommentClient.java
@@ -0,0 +1,17 @@
+package edu.mum.cs544.integration;
+
+import edu.mum.cs544.entities.Book;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@FeignClient(value = "COMMENT", url = "localhost:8080/api")
+public interface CommentClient {
+
+    @GetMapping
+    List<Book> getComment(@RequestParam Long id);
+}
