da86fa604b1328f863c31ccc7adbb9c7f60fb18a
bernietes <bernietes0@gmail.com>
2022-09-22 20:05:38 -0600

    removes unused import

--- Posts/src/main/java/edu/mum/cs544/integration/CommentClient.java
+++ Posts/src/main/java/edu/mum/cs544/integration/CommentClient.java
@@ -3,8 +3,6 @@ package edu.mum.cs544.integration;
 import edu.mum.cs544.entities.Book;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
